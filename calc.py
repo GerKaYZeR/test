f76f0b2c828ff74b150cfa65b3a6a6db6e67340f
GerKaYZeR <104453463+GerKaYZeR@users.noreply.github.com>
2022-05-26 23:07:20 +0600

    1

--- calc.py
+++ calc.py
@@ -1,14 +1,24 @@
 # Примитивный калькуляхтор
-
-print ("калькулятор")
-a = input ("Введите число затем нажмите \"Enter\"\n")
-simbol = input ("Введите символ +, -, *, /.\n затем нажмите \"Enter\"\n")
-b = input ("Введите число\n затем нажмите \"Enter\"\n")
-if simbol == ("+"):
-    print (int (a) + int (b))
-if simbol == ("-"):
-    print (int (a) - int (b))
-if simbol == ("*"):
-    print (int (a) * int (b))
-if simbol == ("/"):
-    print (int (a) / int (b))
+def calc():
+    print ("калькулякТор'т")
+    a = input ("Введите число затем нажмите \"Enter\"\n")
+    simbol = input ("Введите символ +, -, *, /.\nзатем нажмите \"Enter\"\n")
+    b = input ("Введите число\nзатем нажмите \"Enter\"\n")
+    if simbol == ("+"):
+        print (int (a) + int (b))
+    if simbol == ("-"):
+        print (int (a) - int (b))
+    if simbol == ("*"):
+        print (int (a) * int (b))
+    if simbol == ("/"):
+        print (int (a) / int (b))
+        
+    calc()
+    
+while True:
+    q = input ("запустить калькуляхтр? (да/нет): ")
+    
+    if q == ("да"):
+        calc()
+    else:
+        break
